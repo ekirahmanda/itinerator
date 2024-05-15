5961949f0c793375ce9ef8a1df8abffd4aee87f1
irinalalp <irinalaifa@gmail.com>
2024-05-15 23:41:38 +0800

    update UI temporary

--- src/app/api/tripgenerator/route.js
+++ src/app/api/tripgenerator/route.js
@@ -16,16 +16,25 @@ export async function POST(req) {
   } = await req.json();
 
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-4o",
     messages: [
       {
         role: "system",
-        content:
-          'You are an assistant who will provide trip itinerary. Give a reasonable recommendation to user.\n\nIMPORTANT!\nCheck first the inputed budget if the amount make sense. If the amount does not make sense the value isBudget FALSE.\nfor example idr 3 is very small, that amount can\'t be used to buy anything. Follow the currency\n\nthe output should be :\n\n{\nisBudget : false\n}\n\nif isBudget is true, continue this :\n\nConsider these important metrics :\n\n1. The place recommendation should be interesting.\n2. Divide the activity based on the duration given.\n3. Divide activity from morning, afternoon and evening. Depending on how many activity that user wants.\n4. Allocate budget from user to accomodate the whole trip.\n5. Strictly only provide activities based on user limitation activity per day.\n6. Strictly only provide itinerary based on type of activity.\n\nThe output should be in valid json.\n\nexample :\n{\n"trip": "Culinary and Kid-Friendly Trip to Jakarta (1 day)",\n"duration": "1 day",\n"number_of_travelers": 3,\n"total_budget": "IDR 3,000,000",\n"itinerary": {\n"morning": [\n{"activity": "Visit Taman Mini Indonesia Indah", "budget_per_person": "IDR 50,000", "total_budget": "IDR 150,000"}\n],\n"afternoon": [\n{"activity": "Explore Jakarta Aquarium", "budget_per_person": "IDR 100,000", "total_budget": "IDR 300,000"}\n],\n"night": [\n{"activity": "Visit Ancol Dreamland", "budget_per_person": "IDR 100,000", "total_budget": "IDR 300,000"}\n]\n}\n}',
+        content: [
+          {
+            type: "text",
+            text: 'You are an assistant who will provide trip itinerary. Give a reasonable recommendation to user. \n\nConsider these important metrics :  \n1. The place recommendation should be interesting. \n2. Divide the activity based on the duration given. \n3. Divide activity from morning, afternoon and evening. Depending on how many activity that user wants. 4. Allocate budget from user to accomodate the whole trip. \n5. Strictly only provide activities based on user limitation activity per day. \n6. Strictly only provide itinerary based on type of activity.  \n\nIMPORTANT :\nTHE OUTPUT SHOULD BE ONLY VALID JSON WITH FOLLOWING SHAPE \n\nexample : { "trip": "Culinary and Kid-Friendly Trip to Jakarta (1 day)", "duration": "1 day", "number_of_travelers": 3, "total_budget": "IDR 3,000,000", "itinerary": { "morning": [ {"activity": "Visit Taman Mini Indonesia Indah", "budget_per_person": "IDR 50,000", "total_budget": "IDR 150,000"} ], "afternoon": [ {"activity": "Explore Jakarta Aquarium", "budget_per_person": "IDR 100,000", "total_budget": "IDR 300,000"} ], "night": [ {"activity": "Visit Ancol Dreamland", "budget_per_person": "IDR 100,000", "total_budget": "IDR 300,000"} ] } }\n\nIMPORTANT :\nCHECK IF THE BUDGET GIVEN FROM USER CAN\'T ACCOMODATE COMPARE WITH YOUR OWN BUDGET ESTIMATION, RETURN THE OUTPUT TO BE FALSE :\n{\nisBudget : boolean\n}',
+          },
+        ],
       },
       {
         role: "user",
-        content: `I want to go to ${city} for ${duration} days. There are ${number_of_people} people in this trip, my budget is ${currency} ${budget}. I want to have ${number_of_activity} activities per day.  This trip main focus is ${type_of_activity}.\n`,
+        content: [
+          {
+            type: "text",
+            text: `I want to go to ${city} for ${duration} days. There are ${number_of_people} people in this trip, my budget is ${currency} ${budget}. I want to have ${number_of_activity} activities per day.  This trip main focus is ${type_of_activity}. `,
+          },
+        ],
       },
     ],
     temperature: 1,
---
+++ src/app/components/inputUser.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+
+export const InputUser = () => {
+  const [result, setResult] = useState(null);
+
+  async function handleGenerateRecipe(formData) {
+    const city = formData.get("city");
+    const duration = formData.get("duration");
+    const number_of_people = formData.get("number_of_people");
+    const currency = formData.get("currency");
+    const budget = formData.get("budget");
+    const number_of_activity = formData.get("number_of_activity");
+    const type_of_activity = formData.get("type_of_activity");
+
+    const res = await fetch("/api/tripgenerator", {
+      method: "POST",
+      body: JSON.stringify({
+        city,
+        duration,
+        number_of_people,
+        currency,
+        budget,
+        number_of_activity,
+        type_of_activity,
+      }),
+    });
+
+    const data = await res.json();
+    const parsedData = JSON.parse(data.choices[0].message.content);
+    // setResult(parsedData);
+    console.log(parsedData);
+  }
+  return (
+    <main className="max-w-md m-auto my-12 space-y-7">
+      <form className="space-y-2">
+        <textarea
+          name="city"
+          placeholder="city"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="duration"
+          placeholder="duration"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="number_of_people"
+          placeholder="number of people"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="currency"
+          placeholder="currency"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="budget"
+          placeholder="budget"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="number_of_activity"
+          placeholder="number of activity"
+          className="block border p-3 w-full"
+        ></textarea>
+        <textarea
+          name="type_of_activity"
+          placeholder="type of activity"
+          className="block border p-3 w-full"
+        ></textarea>
+        <button>Generate My Trip</button>
+      </form>
+    </main>
+  );
+};
