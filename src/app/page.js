6606da91ac9c321e3c290768b6bdfad2db61bb15
Rizki Rahmanda <eki.rahmanda@gmail.com>
2024-05-16 22:13:15 +0700

    init homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
 export default function Home() {
-  return <div>Ini Homepage</div>;
+  return (
+    <div className="w-5/6 content-center mx-auto">
+      <Header />
+      Ini konten
+      <Footer />
+    </div>
+  );
 }
