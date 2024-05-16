55ca144d905c0cecdd7390c175e9874631b66f4e
bagasanin000 <bagasanindhito@gmail.com>
2024-05-16 19:28:13 +0700

    fixing auth page ui and adding custom font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "@/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"], variable: "--font-nunito" });
 
 export const metadata = {
   title: "Itinerator.ai",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { useState, useRef } from "react";
 import Link from "next/link";
 
+import Image from "next/image";
+
 export const Register = () => {
   const router = useRouter();
   const [message, setMessage] = useState("");
@@ -34,36 +36,74 @@ export const Register = () => {
   }
 
   return (
-    <main className="h-screen grid grid-cols-2">
-      <form
-        action={handleRegister}
-        className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-2"
-      >
-        <section>
-          <h1 className="text-2xl font-medium tracking-tight">
-            Create an Account
-          </h1>
-        </section>
-        <input name="firstName" placeholder="First Name" />
-        <input name="lastName" placeholder="Last Name" />
-        <input name="username" placeholder="Username" />
-        <input name="email" placeholder="Email" />
-        <input name="password" placeholder="Password" type="password" />
-        <button>Register</button>
-      </form>
-      <div className="bg-secondary flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold space-y-2">
-          Keep connected with us!
-        </h1>
-        <h3 className="text-l font-semibold space-y-2">
-          Already have an account?
-        </h3>
-        <Link href="/login">
-          <button>Login</button>
-        </Link>
+    <main className="font-nunito w-full h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full bg-white flex flex-col p-20 justify-center">
+        <div className="w-full flex flex-col">
+          <div className="w-full flex flex-col mb-4">
+            <h3 className="text-4xl font-semibold text-[#0378A6]">
+              Create an account
+            </h3>
+          </div>
+          <form
+            action={handleRegister}
+            className="w-full flex flex-col justify-center items-center rounded-xl space-y-2 mb-5 max-w-full"
+          >
+            <input
+              name="firstName"
+              placeholder="First Name"
+              className="text-sm"
+            />
+            <input
+              name="lastName"
+              placeholder="Last Name"
+              className="text-sm"
+            />
+            <input name="username" placeholder="Username" className="text-sm" />
+            <input name="email" placeholder="Email" className="text-sm" />
+            <input
+              name="password"
+              placeholder="Password"
+              type="password"
+              className="text-sm"
+            />
+            <button className="bg-[#0378A6] hover:bg-primary">Register</button>
+          </form>
+
+          <div className="w-full flex items-center justify-center py-2 mb-2">
+            <div className="w-full h-[0.5px] bg-gray-500"></div>
+          </div>
+
+          <div className="w-full flex items-center justify-between">
+            <p className="text-sm font-normal">Already have an account?</p>
+            <Link
+              href="/login"
+              className="text-sm font-medium underline underline-offset-2 cursor-pointer hover:text-primary "
+            >
+              Login to your account
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="text-sm">
-        {message !== "" ? <div>{message}</div> : null}
+      <div className="relative h-full flex flex-col hidden lg:block">
+        <div className="absolute top-[28%] right-[10%] flex flex-col">
+          <h1 className="text-7xl text-slate-800 font-extrabold filter blur-px my-4 text-right">
+            Your journey starts <br /> here!
+          </h1>
+          <p className="text-xl text-gray-900 font-normal filter blur-px mb-4 text-right">
+            Start your journey from preparing the itinerary well
+          </p>
+        </div>
+        <div class="flex w-full h-full">
+          <div class="bg-gradient-to-tr from-[#F2AEDB] to-[#0378A6] max-w-full overflow-hidden ">
+            <Image
+              src="/bg-people.jpg"
+              width={1000}
+              height={1000}
+              alt="background image"
+              className="mix-blend-overlay relative w-full h-full object-cover"
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
