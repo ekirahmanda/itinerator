252cc73395695e8b2143eb1499b52f67b5c1b23a
irinalalp <irinalaifa@gmail.com>
2024-05-25 11:55:40 +0800

    modifying the country button and share button to be generate new trip

--- src/components/inputUser.jsx
+++ src/components/inputUser.jsx
@@ -10,6 +10,7 @@ import "react-country-state-city/dist/react-country-state-city.css";
 export const InputUser = () => {
   const [result, setResult] = useState(null);
   const formRef = useRef(null);
+  const [selectedCountry, setSelectedCountry] = useState("");
   let dayCounter = 1;
   console.log(result);
   const currencyOptions = [
@@ -22,7 +23,10 @@ export const InputUser = () => {
   const [selectedCity, setSelectedCity] = useState("");
 
   async function handleGenerateTrip(formData) {
-    const city = selectedCity;
+    let city = selectedCity;
+    if (!city && !stateid) {
+      city = selectedCountry;
+    }
     const duration = formData.get("duration");
     const numberOfPeople = formData.get("numberOfPeople");
     const currency = formData.get("currency");
@@ -67,138 +71,127 @@ export const InputUser = () => {
       },
     });
   }
+  const handleNewTrip = () => {
+    setResult(null); // Clear previous trip results
+    formRef.current.reset(); // Reset form fields
+    setCountryid(0); // Reset country select
+    setstateid(0); // Reset state select
+    setSelectedCity(""); // Reset selected city
+  };
 
   return (
     <main className="max-w-md m-auto my-12 space-y-7">
-      <h1>Let&lsquo;s pack your bag!</h1>
-      <Link href="/dashboard">
-        <button className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:outline-none">
-          My Saved Generated Trip Lists
-        </button>
-      </Link>
-      <form ref={formRef} action={handleGenerateTrip} className="space-y-2">
-        <div className="grid grid-cols-3 gap-4">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Plan Your Next Adventure!
+      </h1>
+      <form ref={formRef} action={handleGenerateTrip} className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <CountrySelect
             countryid={countryid}
             onChange={(e) => {
               setCountryid(e.id);
+              setSelectedCountry(e.name); // Update selected country name
             }}
             placeHolder="Select Country"
           />
-          <div>
-            <StateSelect
-              name="city"
-              countryid={countryid}
-              onChange={(e) => {
-                setSelectedCity(e.name); // Update the selected city
-                setstateid(e.id);
-              }}
-              placeholder="City"
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="duration"
-              placeholder="Duration"
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              name="numberOfPeople"
-              placeholder="Number of People"
-              min="1"
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div>
-            <Select
-              name="currency"
-              placeholder="Select Currency"
-              options={currencyOptions} // Define currency options
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              name="budget"
-              placeholder="Budget"
-              min="1"
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div>
-            <input
-              type="number"
-              name="numberOfActivity"
-              placeholder="Number of Activities"
-              min="1"
-              className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            />
-          </div>
-          <div className="col-span-3">
-            <select
-              name="typeOfActivity"
-              defaultValue=""
-              className="select select-primary w-full max-w-xs block p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
-            >
-              <option value="" disabled>
-                What is your activity type plan?
-              </option>
-              <option value="Culinary">Culinary</option>
-              <option value="History and Culture">History and Culture</option>
-              <option value="Staycation">Staycation</option>
-              <option value="Outdoor Activity">Outdoor Activity</option>
-            </select>
-          </div>
+          <StateSelect
+            name="city"
+            countryid={countryid}
+            onChange={(e) => {
+              const cityName = e.name || selectedCountry; // If state is empty, use the selected country name
+              setSelectedCity(cityName);
+              setstateid(e.id);
+            }}
+            placeholder="Select City"
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <input
+            type="text"
+            name="duration"
+            placeholder="Duration (e.g., 3 days)"
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <input
+            type="number"
+            name="numberOfPeople"
+            placeholder="Number of People"
+            min="1"
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <Select
+            name="currency"
+            placeholder="Select Currency"
+            options={currencyOptions}
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <input
+            type="number"
+            name="budget"
+            placeholder="Budget"
+            min="1"
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <input
+            type="number"
+            name="numberOfActivity"
+            placeholder="Number of Activities"
+            min="1"
+            className="block w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          />
+          <select
+            name="typeOfActivity"
+            defaultValue=""
+            className="select select-primary w-full p-3 border border-gray-300 rounded-md focus:ring focus:ring-blue-300 focus:outline-none"
+          >
+            <option value="" disabled>
+              What is your activity type plan?
+            </option>
+            <option value="Culinary">Culinary</option>
+            <option value="History and Culture">History and Culture</option>
+            <option value="Staycation">Staycation</option>
+            <option value="Outdoor Activity">Outdoor Activity</option>
+          </select>
         </div>
-        <button className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:outline-none">
+        <button className="w-full bg-blue-500 text-white font-semibold py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
           Generate My Trip
         </button>
       </form>
-      {result ? (
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-bold">{result.trip}</h2>
-          </div>
+      {result && (
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-4">{result.trip}</h2>
           <div>
-            {result.activities.map((activity, index) => {
-              return (
-                <div key={index} className="space-y-4">
-                  <h3 className="text-xl font-bold">Day : {activity.day}</h3>
-                  <div className="space-y-4">
-                    {activity.activitiesOnDay.map((item) => {
-                      return (
-                        <div key={item.time}>
-                          <div>Time : {item.time}</div>
-                          <div>Activity : {item.activity}</div>
-                          <div>Budget Per Person : {item.budgetPerPerson}</div>
-                          <div>Total Budget : {item.totalBudget}</div>
-                        </div>
-                      );
-                    })}
-                  </div>
+            {result.activities.map((activity, index) => (
+              <div key={index} className="mb-6">
+                <h3 className="text-xl font-bold">Day {index + 1}</h3>
+                <div className="space-y-2">
+                  {activity.activitiesOnDay.map((item, i) => (
+                    <div key={i}>
+                      <p className="font-semibold">Time: {item.time}</p>
+                      <p>Activity: {item.activity}</p>
+                      <p>Budget Per Person: {item.budgetPerPerson}</p>
+                      <p>Total Budget: {item.totalBudget}</p>
+                    </div>
+                  ))}
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
-          <div className="flex space-x-4 mt-12">
+          <div className="flex justify-end mt-6 space-x-4">
             <button
               onClick={handleSaveTrip}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded focus:outline-none"
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
             >
               Save
             </button>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded focus:outline-none">
-              Share
+            <button
+              onClick={handleNewTrip}
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+            >
+              Generate New Trip
             </button>
           </div>
         </div>
-      ) : null}
+      )}
     </main>
   );
 };
